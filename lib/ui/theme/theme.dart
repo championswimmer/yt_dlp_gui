caa61347cdcdf29158f81333d390add97873ec65
Arnav Gupta <championswimmer@gmail.com>
2023-07-17 21:29:46 +0530

    [cleanup] we do not need separate font sizes right now
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- lib/ui/theme/theme.dart
+++ lib/ui/theme/theme.dart
@@ -4,51 +4,6 @@ class AppTheme {
   const AppTheme._();
   static ThemeData get darkTheme => ThemeData.from(
         useMaterial3: true,
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Colors.white,
-            fontSize: 48,
-            fontWeight: FontWeight.w600,
-          ),
-          displayMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-            fontWeight: FontWeight.w500,
-          ),
-          displaySmall: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-          ),
-          headlineMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-          ),
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
-          headlineSmall: TextStyle(
-            color: Colors.white,
-            fontSize: 12,
-            fontWeight: FontWeight.w700,
-          ),
-          titleMedium: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w300,
-            fontSize: 12,
-          ),
-          titleSmall: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w300,
-            fontSize: 16,
-          ),
-        ),
         // TODO: we can customize the color scheme here
         colorScheme: const ColorScheme.dark(primary: Colors.redAccent),
       );
